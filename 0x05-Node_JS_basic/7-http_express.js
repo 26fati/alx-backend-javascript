1cc66c9669f966e8befaf6ca61517479808bd572
26fati <fatibeautiful1@gmail.com>
2024-04-30 16:04:26 +0100

    fix eslint

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async')
+const countStudents = require('./3-read_file_async');
 
 const port = 1245;
 
@@ -8,19 +8,18 @@ const app = express();
 app.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
-})
+});
 
 app.get('/students', (req, res) => {
-    res.write('This is the list of our students\n');
-    try {
-        countStudents(process.argv[2].toString()).then((output)=> {
-            res.end(output);
-        });
-        
-    } catch (error) {
-        res.end(error);
-    }
-})
+  res.write('This is the list of our students\n');
+  try {
+    countStudents(process.argv[2].toString()).then((output) => {
+      res.end(output);
+    });
+  } catch (error) {
+    res.end(error);
+  }
+});
 
 app.listen(port, () => {
 
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
+
 const app = express();
-const router = require('./router');
-const port = 1245
+const router = require('./routes/index');
+
+const port = 1245;
 
 app.use('/', router);
 app.use('students', router);
 app.use('/students/:major', router);
 
-app.listen(port)
+app.listen(port);
 
 export default app;

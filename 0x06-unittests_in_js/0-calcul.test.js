b1f5adf9d314809568c044b400b60de86472701d
26fati <fatibeautiful1@gmail.com>
2024-05-02 16:17:28 +0100

    test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,7 +4,7 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
   it('should return the sum of two rounded numbers', () => {
     assert.strictEqual(calculateNumber(1.4, 2.6), 4);
-    assert.strictEqual(calculateNumber(1.5, 2.5), 4);
+    assert.strictEqual(calculateNumber(1.5, 2.5), 5);
     assert.strictEqual(calculateNumber(1.6, 2.4), 4);
   });
 });
